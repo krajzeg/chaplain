91233af788605c74ac0107b39113e6cff9798931
Jakub Wasilewski <wasyl@wasyl.eu>
2016-05-31 18:22:02 +0200

    Reintroduce Chaplain on Chaplain tests, now reproducible between versions/runs.

--- test-chaplain/chaplain.tests.js
+++ test-chaplain/chaplain.tests.js
@@ -5,6 +5,16 @@ import path from 'path';
 // This gives a bit of dogfooding, and as a bonus, Chaplain works well for
 // testing text output of CLI tools.
 suite("chaplain", () => {
+  config({
+    text: {
+      ignoreWords: ['[.+?!]+'], // test order is not reproducible between runs, so the "Running tests .?!!..."
+                                // progress indicator has to be excluded
+
+      ignoreLines: ["\\s+at .*\\d+:\\d+.*"] // exact stacktrace lines are not reproducible between node versions/runs
+    }
+  });
+
+  // test a suite that produces every possible type of result
   test("every-result-type", () => runCLIAndGetOutput({
     files: path.join(process.cwd(), 'test-data/every-result-type'),
     args: ['-IC', '-f', 'every-result-type.chaplain.js', '-d', './']
