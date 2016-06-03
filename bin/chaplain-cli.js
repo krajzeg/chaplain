0227b4d3ce9c908d94d37d9e96b0f62d73e09e72
Jakub Wasilewski <wasyl@wasyl.eu>
2016-06-03 16:03:40 +0200

    Rewrite the binary in ES5 so it is no longer a generated file.

--- bin/chaplain-cli.js
+++ bin/chaplain-cli.js
@@ -1,15 +1,16 @@
 #!/usr/bin/env node
-/* jshint node: true, esversion: 6 */
-"use strict";
+
+// This file is in ES5 - having executable files that are generated
+// by babel each time is a major pain in the neck.
 
 // The entire CLI is instantiated by passing the "real world" into it.
 // This enables testing of the entire CLI application using mocks of
 // the filesystem, input and output.
-import inquirer from 'inquirer';
-import fs from 'fs';
-import cliEngine from './cli-engine';
+var inquirer = require('inquirer');
+var fs = require('fs');
+var cliEngine = require('../dist/lib/cli/cli-engine').default;
 
-const engine = cliEngine({
+var engine = cliEngine({
   // system
   argv: process.argv,
   // FS
@@ -24,7 +25,8 @@ const engine = cliEngine({
 // The entire job of the engine is to run whatever was needed,
 // and return an exit code for the system.
 engine.run()
-  .then(() => process.exit(0))
-  .catch(err => {
+  .then(function(exitCode) { return process.exit(exitCode); })
+  .catch(function(err) {
     console.error("ERROR IN ENGINE: ", err);
+    process.exit(2);
   });
